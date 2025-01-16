f4d8c23079452376a7f542e52107a4b689a65c80
ISD04 <GAJJALA.20211ISD0015@presidencyuniversity.in>
2025-01-16 16:13:08 +0530

    Delete db.sql

--- db.sql
+++
@@ -1,9 +0,0 @@
-DROP DATABASE IF EXISTS fire_detection_system;
-CREATE DATABASE fire_detection_system;
-USE fire_detection_system;
-
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
